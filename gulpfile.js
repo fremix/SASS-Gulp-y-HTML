46e4a3ceec550858144361d45e9a6d7462829ea2
fr3m1 <microsys.net@gmail.com>
2023-03-21 13:32:21 -0600

    Proyecto Festival Musica

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const { src, dest, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+function css(done) {
+  src("src/scss/app.scss") /** Identificar el archivo SASS */
+    .pipe(sass()) /** Compilarlo */
+    .pipe(dest("build/css")); /** Almacenarlo en el DD */
+
+  done();
+}
+
+function dev(done) {
+  watch("src/scss/app.scss", css);
+  done();
+}
+
+exports.css = css;
+exports.dev = dev;
